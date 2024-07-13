12943852b5f5ddf9cec0a62bca8e8e5a770fc809
Niranjan A S <niranjan0881@gmail.com>
2024-07-14 01:51:40 +0530

    chore: changed the map path

--- src/pages/kerala-map.tsx
+++ src/pages/kerala-map.tsx
@@ -1,12 +1,12 @@
 import { memo } from "react";
-import { useFetch } from "../hooks/use-fetch.ts";
 import { MapWithMarker } from "../components/map/map-with-marker.tsx";
+import { useFetch } from "../hooks/use-fetch.ts";
 import { IMapWithMarkerProps as IMarkerMapOptions } from '../types/index.tsx';
 
 
 export default memo(function KeralaApp() {
 
-    const mapData = useFetch<IMarkerMapOptions>('/marker-map.json');
+    const mapData = useFetch<IMarkerMapOptions>('/map-data/marker.json');
 
     return (
         (mapData) ? <MapWithMarker {...mapData} /> : 'Loading...'
--- src/pages/usa-map.tsx
+++ src/pages/usa-map.tsx
@@ -1,12 +1,12 @@
 import { memo } from 'react';
+import { MapWithPolygon } from '../components/map/map-with-polygon';
 import { useFetch } from '../hooks/use-fetch';
 import { IMapWithPolygonProps } from '../types';
-import { MapWithPolygon } from '../components/map/map-with-polygon';
 
 
 export default memo(function USAMap() {
 
-    const mapData = useFetch<IMapWithPolygonProps>('/polygon-map.json');
+    const mapData = useFetch<IMapWithPolygonProps>('/map-data/polygon.json');
 
     return (
         (mapData) ? <MapWithPolygon {...mapData} /> : 'Loading...'
