7e5d44b2531db7c67435af65f4fe3965edc853ad
Niranjan A S <niranjan0881@gmail.com>
2024-07-14 03:53:18 +0530

    chore: updated the header

--- src/components/home/header.tsx
+++ src/components/home/header.tsx
@@ -1,26 +1,12 @@
 import { FC, memo } from "react";
 import { Link } from "react-router-dom";
 import { MapIcon } from "../map/map-icon";
-import { BANNER_DATA } from "../../constants";
 
 export const Header: FC = memo(() => (
     <header className="bg-black text-white px-4 lg:px-6 h-14 flex items-center justify-between">
         <Link to="/" className="flex items-center justify-center" >
             <MapIcon className="h-6 w-6" />
+            <span className="ml-2 text-xl font-bold">Geotrail</span>
         </Link>
-        <nav className="flex gap-4 sm:gap-6">
-            <Link
-                to="/map/kerala"
-                className="text-sm font-medium hover:underline underline-offset-4"
-            >
-                {BANNER_DATA.kerala.title}
-            </Link>
-            <Link
-                to="/map/usa"
-                className="text-sm font-medium hover:underline underline-offset-4"
-            >
-                {BANNER_DATA.usa.title}
-            </Link>
-        </nav>
     </header>
 ))
