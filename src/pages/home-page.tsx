5f09fba14284f0fa81be44a22379c5d8dba95289
Niranjan A S <niranjan0881@gmail.com>
2024-07-14 12:49:02 +0530

    chore: updated the homepage

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -1,22 +1,26 @@
-import { memo } from 'react';
+import { memo, useCallback } from 'react';
 import { Banner } from '../components/home/banner';
 import { Footer } from '../components/home/footer';
 import { Header } from '../components/home/header';
 import { BANNER_DATA } from '../constants';
+import { IBannerProps } from '../types';
 
-export default memo(() => (
-    <div className="flex flex-col h-screen">
-        <Header />
-        <main className="flex-1">
-            <div className="container px-4 md:px-6 py-12 md:py-24 lg:py-32">
-                <div className="grid gap-8 md:grid-cols-2 lg:grid-cols-2">
-                    <Banner {...BANNER_DATA.kerala} />
-                    <Banner {...BANNER_DATA.usa} />
-                    <Banner {...BANNER_DATA.current} />
+export default memo(() => {
+
+    const renderBanners = useCallback((data: IBannerProps) => <Banner {...data} />, []);
+
+    return (
+        <div className="flex flex-col h-screen">
+            <Header />
+            <main className="flex-1">
+                <div className="container px-4 md:px-6 py-12 md:py-24 lg:py-32">
+                    <div className="grid gap-8 md:grid-cols-3 lg:grid-cols-3">
+                        {Object.values(BANNER_DATA).map(renderBanners)}
+                    </div>
                 </div>
-            </div>
-        </main>
-        <Footer />
-    </div>
-));
+            </main>
+            <Footer />
+        </div>
+    );
+});
 
