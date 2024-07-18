596f410f34c7fafacdcdd40171da149240865fd6
Niranjan A S <niranjan0881@gmail.com>
2024-07-18 18:24:03 +0530

    chore: changed the header styles

--- src/components/home/header.tsx
+++ src/components/home/header.tsx
@@ -4,7 +4,7 @@ import { MapIcon } from '../map/map-icon';
 import { ROUTE_PATHS } from '../../constants';
 
 export const Header: FC = memo(() => (
-    <header className="bg-black text-white px-4 lg:px-6 h-14 flex items-center justify-between">
+    <header className="bg-black text-white px-4 lg:px-6 py-2 flex items-center justify-between">
         <Link to={ROUTE_PATHS.home} className="flex items-center justify-center" >
             <MapIcon className="h-6 w-6" />
             <span className="ml-2 text-xl font-bold">Geotrail</span>
