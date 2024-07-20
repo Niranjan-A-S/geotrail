27a1f43bb700f44c7cd3071d3e4fbbad02782cff
Niranjan A S <niranjan0881@gmail.com>
2024-07-20 12:54:15 +0530

    chore: made the header fixed

--- src/components/home/header.tsx
+++ src/components/home/header.tsx
@@ -4,7 +4,7 @@ import { MapIcon } from '../map/map-icon';
 import { APP_NAME, ROUTE_PATHS } from '../../constants';
 
 export const Header: FC = memo(() => (
-    <header className="bg-black text-white px-4 lg:px-6 py-2 flex items-center justify-between">
+    <header className="bg-black text-white px-4 lg:px-6 py-2 flex items-center justify-between fixed left-0 right-0">
         <Link to={ROUTE_PATHS.home} className="flex items-center justify-center" >
             <MapIcon className="h-6 w-6" />
             <span className="ml-2 text-xl font-bold">{APP_NAME}</span>
